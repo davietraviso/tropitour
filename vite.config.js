e522df7143d2c6be5dd2a218a12f446ed6027c41
Dave Travis <163664552+davietraviso@users.noreply.github.com>
2024-12-08 23:21:46 +0700

    base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Use '/' for root or '/subpath/' if deploying to a subdirectory
 })
